597606fca8684028bd25d41a9d827bf5288af33a
David Oved <your-email@example.com>
2026-05-31 16:52:46 +0300

    Test simple handler

---
+++ api/test.py
@@ -0,0 +1,8 @@
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(b'<h1>Test OK</h1>')
